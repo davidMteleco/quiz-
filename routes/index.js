042ba60bb2513f2510bcd72b23f36ed09db42624
davidMteleco <david_ok10@hotmail.es>
2016-05-05 12:42:48 +0200

    Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,9 @@ router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 router.get('/quizzes/new',                 quizController.new);
 router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+
 
 /* GET author. */
 router.get('/author', function(req, res, next) {
