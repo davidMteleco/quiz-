a5c7fc88a453fbffa93c697c92490935902a7af6
davidMteleco <david_ok10@hotmail.es>
2016-05-05 10:57:05 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
 var express = require('express');
 var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
 
 // Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
 
 /* GET author. */
 router.get('/author', function(req, res, next) {
   res.render('author');
 });
 
+
 module.exports = router;
-
-
 
 
