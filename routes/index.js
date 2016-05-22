5bb00722d423034cddd4d9e2e06b7b8df6537076
davidMteleco <david_ok10@hotmail.es>
2016-05-22 14:38:38 +0200

    Añadir imagenes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var multer  = require('multer');
+var upload = multer({ dest: './uploads/' });
+
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var userController = require('../controllers/user_controller');
 var sessionController = require('../controllers/session_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,6 +27,7 @@ router.get('/session',    sessionController.new);     // formulario login
 router.post('/session',   sessionController.create);  // crear sesión
 router.delete('/session', sessionController.destroy); // destruir sesión
 
+
 // Definición de rutas de cuenta
 router.get('/users',                    userController.index);   // listado usuarios
 router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
@@ -38,25 +43,27 @@ router.delete('/users/:userId(\\d+)',   sessionController.loginRequired,
 										sessionController.adminAndNotMyselfRequired, 
 										userController.destroy);  // borrar cuenta
 
-
 // Definición de rutas de /quizzes
+router.get('/quizzes',                     	quizController.index);
 router.get('/quizzes.:format?',                    quizController.index);
 router.get('/quizzes/:quizId(\\d+).:format?',      quizController.show);
-router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/:quizId(\\d+)',       	quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check', 	quizController.check);
 router.get('/quizzes/new',                 	sessionController.loginRequired, 
 											quizController.new);
 router.post('/quizzes',                    	sessionController.loginRequired, 
+											upload.single('image'),
 											quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',  	sessionController.loginRequired, 
 										   	quizController.ownershipRequired, 
 										   	quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       	sessionController.loginRequired, 
 											quizController.ownershipRequired, 
+											upload.single('image'),
 											quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',    	sessionController.loginRequired, 
 											quizController.ownershipRequired, 
 											quizController.destroy);
-
 
 // Definición de rutas de comentarios
 router.get('/quizzes/:quizId(\\d+)/comments/new',  sessionController.loginRequired, 
@@ -68,13 +75,9 @@ router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept',
 	                                               quizController.ownershipRequired, 
 	                                               commentController.accept);
 
-
 /* GET author. */
 router.get('/author', function(req, res, next) {
   res.render('author');
 });
 
-
 module.exports = router;
-
-
